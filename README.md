8e0605e105fb166f6405668fc33e58b1e57450fa
Michael Johansen <michael@redperformance.no>
2018-02-12 20:02:47 +0100

    initial commit of files from previous liberaries

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# measurement-framework
-Collection of Javascript code to implement a browser based measurement system for websites. It uses a cookie to store a users action on a webpage and let you map thoose actions to a particular state in the user journey.
+# Measurement Framework
+Collection of Javascript code to implement a browser based measurement system for websites. It uses a
+cookie to store a users action on a webpage and let you map thoose actions to a particular state in the
+user journey.
+
+
+## Install
+
+
+
+## Cave eats
+Rollup and other javascript compilers doesn't support removing unused methods form classes. This is why the measurement
+framework i built on running several functions instead of importing a class to hold them. Internally these uses the
+MeasurementFramework class to hold state and to orcestrate between classes.
---
+++ rollup.config.js
@@ -0,0 +1,74 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
+import {minify} from "uglify-es";
+import json from "rollup-plugin-json";
+import eslint from "rollup-plugin-eslint";
+import replace from "rollup-plugin-replace";
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+/*
+const babel_preset = {
+    presets: [
+        ["@babel/preset-env", {
+            "targets": {
+                "browsers": ["> 0.5%"]
+            }
+        }]
+    ]
+};
+*/
+export default {
+    input: 'src/user-trail.js',
+    name: "measurementFramework",
+    strict: false,
+    sourcemap: false,
+    output: {
+        file: 'public/bundle.js',
+        format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+        sourcemap: false
+    },
+    treeshake: true,
+    globals: {
+        window: 'window',
+        document: 'document'
+    },
+    plugins: [
+        json(),
+        replace({
+            "RMFBUILDTIME": new Date().toISOString()
+        }),
+        resolve({
+            module: true,       // Default: true
+            jsnext: true,       // Default: false
+            main: true,         // Default: true
+            browser: true,      // Default: false
+            extensions: [
+                '.js',
+                '.json'],           // Default: ['.js']
+            preferBuiltins: true,   // Default: true
+            modulesOnly: false,     // Default: false
+            customResolveOptions: {}
+        }), // tells Rollup how to find date-fns in node_modules
+        commonjs(), // converts date-fns to ES modules
+        eslint({}),
+        babel({
+            exclude: [
+                'node_modules/**',
+                '*.json'
+            ]
+        }),
+        production && uglify({
+            toplevel: true,
+            ie8: true,
+            sourceMap: false,
+            compress: {
+                drop_console: true
+            },
+        }, minify) // minify, but only in production
+    ]
+};
