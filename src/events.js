de7ef863205006ac515eb776204ccaaf784c5589
Asim Tariq <asim@redperformance.no>
2018-08-28 14:38:59 +0200

    Added measurement-framework under src for example case

---
+++ src/events.js
@@ -0,0 +1,51 @@
+export const CONTENT_VIEWED = 'Content Viewed';
+export const RETAIL_OUTLET_VIEWED = 'Retail Outlet Viewed';
+export const PHONE_LINK_CLICKED = 'Phone Link Clicked';
+export const NEWSLETTER_SUBSCRIBED = 'Newsletter Subscribed';
+export const MAILTO_LINK_CLICKED = 'Mailto Link Clicked';
+export const PRODUCT_REQUEST_SENT = 'Product Request Sent';
+export const PRODUCT_VIEWED = 'Product Viewed';
+export const PROMOTION_MAIN_CLICKED = 'Promotion Main Clicked';
+export const RETAIL_MAP_LOCATION = 'Retail Map Location';
+export const SOCIAL_LINK_CLICKED = 'Social Link Clicked';
+export const INTERNAL_SEARCH_RESULTS = 'Results';
+export const INTERNAL_SEARCH_NO_RESULTS = 'No Results';
+export const FINANCIAL_LINK_CLICKED = 'Financial Link Clicked';
+export const CATEGORY_LIST_VIEWED = 'Category List Viewed';
+export const BRAND_LIST_VIEWED = 'Brand List Viewed';
+export const ADD_FILTER_PRICERANGE = 'Add Filter Pricerange';
+export const ASSEMBLY_FILE_DOWNLOADED = 'Assembly File Downloaded'; // Waiting for Developer - dataLayer
+export const PRODUCT_SHARED = 'Product Shared';
+export const EMAGAZINE_LINK_CLICKED = 'EMagazine Link Clicked';
+export const ADD_FILTER_PRODUCT_LIST = 'Product List Filtered';
+export const SORT_BY = 'Sort By';
+export const INSPECTION_FORM_SUBMITTED = 'Inspection Form Submitted';
+export const RETAIL_MAP_ENGAGED = 'Retail Map Engagement';
+
+
+
+export default [
+    CONTENT_VIEWED,
+    RETAIL_OUTLET_VIEWED,
+    PHONE_LINK_CLICKED,
+    NEWSLETTER_SUBSCRIBED,
+    MAILTO_LINK_CLICKED,
+    PRODUCT_REQUEST_SENT,
+    PRODUCT_VIEWED,
+    PROMOTION_MAIN_CLICKED,
+    RETAIL_MAP_LOCATION,
+    SOCIAL_LINK_CLICKED,
+    INTERNAL_SEARCH_RESULTS,
+    INTERNAL_SEARCH_NO_RESULTS,
+    FINANCIAL_LINK_CLICKED,
+    CATEGORY_LIST_VIEWED,
+    BRAND_LIST_VIEWED,
+    ADD_FILTER_PRICERANGE,
+    ASSEMBLY_FILE_DOWNLOADED,
+    PRODUCT_SHARED,
+    EMAGAZINE_LINK_CLICKED,
+    ADD_FILTER_PRODUCT_LIST,
+    SORT_BY,
+    INSPECTION_FORM_SUBMITTED,
+    RETAIL_MAP_ENGAGED
+];
